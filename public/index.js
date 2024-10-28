94caf7e1bb545f1a8b99b2be23bc0c8948b54e46
Maaniksharma <manikss123456@gmail.com>
2024-10-28 21:36:25 +0530

    bug/fix fixed http to https:

--- public/index.js
+++ public/index.js
@@ -1,5 +1,5 @@
 let ws;
-const proto = window.location.protocol === "https" ? "wss" : "ws";
+const proto = window.location.protocol === "https:" ? "wss" : "ws";
 ws = new WebSocket(`${proto}://${window.location.host}`);
 ws.onmessage = (event) => {
   console.log(event.data);
